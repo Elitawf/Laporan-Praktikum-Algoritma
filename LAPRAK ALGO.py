f79f761c4e5cf904b04cd430c654e7395800859f
Elitawf <065002200038@trisakti.ac.id>
2022-09-22 10:03:57 +0700

    Create LAPRAK ALGO.py

---
+++ LAPRAK ALGO.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 22 09:44:09 2022
+
+@author: Elita
+"""
+
+quest1 = int(input("Halo User! Silahkan masukkan panjang ruangan yang diinginkan: "))
+quest2 = int(input("Silahkan user masukkan lebar ruangan yang diinginkan: "))
+quest3 = input("Silahkan masukkan satuan yang diinginkan Meter / Inch: ")
+Jumlah = quest1 * quest2
+
+print (f"hasil dari luas ruangan anda adalah {Jumlah} {quest3}")
